7b1293c9a9fca835d77442416a6591a23bb9d71e
Elon Gliksberg <elongliks@gmail.com>
2022-10-07 00:17:16 +0300

    Using inspect.getattr_static() instead of getattr().

--- scan.py
+++ scan.py
@@ -1,3 +1,4 @@
+import inspect
 from collections.abc import Iterable, Mapping
 from typing import Tuple
 
@@ -9,14 +10,14 @@ def _get_obj_prop_items(obj, only_public_props: bool, scan_path: str):
     items = []
     for prop in dir(obj):
         try:
-            val = getattr(obj, prop)
+            val = inspect.getattr_static(obj, prop)
             if only_public_props and prop.startswith('_'):
                 continue
             if prop.startswith('__') or callable(val):
                 continue
             items.append((prop, val))
         except Exception as err:
-            print(f'Unable to scan {scan_path}/{prop}: {err}')
+            print(f'Unable to scan {scan_path}.{prop}: {err}')
     return items
 
 
@@ -28,7 +29,7 @@ def scan(
         max_depth: int = 5,
         depth: int = 0,
         only_public_props: bool = False,
-        is_initial_run: bool = True
+        is_initial_run: bool = True,
 ):
     if is_initial_run:
         _scanned_ids.clear()
@@ -46,23 +47,25 @@ def scan(
         for key, val in obj.items():
             if only_public_props and str(key).startswith('_'):
                 continue
-            scan(val, scan_values, scan_types, f'{scan_path}/{key}', max_depth, depth + 1, only_public_props,
+            scan(val, scan_values, scan_types, f'{scan_path}.{key}', max_depth, depth + 1, only_public_props,
                  is_initial_run=False)
         return
     if isinstance(obj, Iterable):
         for val in obj:
-            scan(val, scan_values, scan_types, f'{scan_path}/{val}', max_depth, depth + 1, only_public_props,
+            scan(val, scan_values, scan_types, f'{scan_path}.{val}', max_depth, depth + 1, only_public_props,
                  is_initial_run=False)
         return
 
     if obj in scan_values:
         print(f'[*] Found value @ {scan_path}')
+        _scanned_ids.remove(id(obj))
     if isinstance(obj, scan_types):
         print(f'[*] Found type @ {scan_path}')
+        _scanned_ids.remove(id(obj))
 
     for prop_key, prop_val in _get_obj_prop_items(obj, only_public_props, scan_path):
         try:
-            scan(prop_val, scan_values, scan_types, f'{scan_path}/{prop_key}', max_depth, depth + 1, only_public_props,
+            scan(prop_val, scan_values, scan_types, f'{scan_path}.{prop_key}', max_depth, depth + 1, only_public_props,
                  is_initial_run=False)
         except Exception as err:
-            print(f'Unable to scan {scan_path}/{prop_key}: {err}')
+            print(f'Unable to scan {scan_path}.{prop_key}: {err}')
