2eead94ae070c46fbc4339952803b2010c08ff39
Elon Gliksberg <elongliks@gmail.com>
2022-10-06 17:34:41 +0300

    Added the script.

---
+++ scan.py
@@ -0,0 +1,42 @@
+import sys
+from collections.abc import Iterable, Mapping
+from typing import Any, List, Tuple
+
+
+PRIMITIVE_TYPES = (int, str, float, bool)
+scanned_ids = set()
+
+
+def get_obj_prop_items(obj):
+    items = []
+    for prop in dir(obj):
+        val = getattr(obj, prop)
+        if prop.startswith('__') or callable(val):
+            continue
+        items.append((prop, val))
+    return items
+
+
+def scan(obj, values: Iterable, types: Tuple, path: str = ''):
+    if id(obj) in scanned_ids:
+        return
+    scanned_ids.add(id(obj))
+    if isinstance(obj, PRIMITIVE_TYPES):
+        return
+    if isinstance(obj, Mapping):
+        for key, val in obj.items():
+            search(val, values, types, f'{path}/{key}')
+        return
+    if isinstance(obj, Iterable):
+        for val in obj:
+            search(val, values, types, f'{path}/{obj}')
+        return
+
+    if obj in values:
+        print(f'Found value: {obj} @ {path}')
+    if isinstance(obj, types):
+        print(f'Found type: {obj} @ {path}')
+
+    for prop_key, prop_val in get_obj_prop_items(obj):
+        search(prop_val, values, types, f'{path}/{prop_key}')
+
